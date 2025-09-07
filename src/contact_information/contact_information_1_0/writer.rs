af6e1b8f40bceba260a330131710d71bba6d55e6
NoName11234 <ykonto91@gmail.com>
2025-09-08 00:04:22 +0200

    added basic module structure

---
+++ src/contact_information/contact_information_1_0/writer.rs
@@ -0,0 +1,11 @@
+use aas_model_rs::structs::submodel::Submodel;
+
+pub struct ContactInformationWriter1_0 {
+    submodel: Submodel
+}
+
+impl ContactInformationWriter1_0 {
+    pub fn new() -> ContactInformationWriter1_0 {
+        todo!()
+    }
+}
